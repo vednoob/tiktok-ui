cf579966001acdde0323d136b0392b23008669cd
vednoob <leduchyy@gmail.com>
2023-02-10 22:54:45 +0700

    Build UI for header - logo and search

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,9 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
 import styles from './Header.module.scss';
+import images from '~/assets/images';
 
 // do not clear this line -> bind object styles to classNames and return a function to cx.
 // So when using cx to create a claas
@@ -10,8 +14,24 @@ function Header() {
   return (
     <header className={cx('wrapper')}>
       <div className={cx('inner')}>
-        {/* {Logo} */}
-        {/* {Search} */}
+        <div className={cx('logo')}>
+          <img src={images.logo} alt="Tiktok" />
+        </div>
+
+        <div className={cx('search')}>
+          <input placeholder="Search accounts and videos" spellCheck={false} />
+          <button className={cx('clear')}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+          <button className={cx('search-btn')}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+
+        <div className={cx('action')}>
+          <h2>Action here</h2>
+        </div>
       </div>
     </header>
   );
