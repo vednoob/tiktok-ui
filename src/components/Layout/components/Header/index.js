5a8f3e9c8fc731e74f6bf3b253066dea5fa3e650
vednoob <leduchyy@gmail.com>
2023-02-10 17:36:53 +0700

    Build Project Base - Default Layout

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,20 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+// do not clear this line -> bind object styles to classNames and return a function to cx.
+// So when using cx to create a claas
+// , this help us create class using "-"  class-name
+const cx = classNames.bind(styles);
+
 function Header() {
-  return <h2>Header</h2>;
+  return (
+    <header className={cx('wrapper')}>
+      <div className={cx('inner')}>
+        {/* {Logo} */}
+        {/* {Search} */}
+      </div>
+    </header>
+  );
 }
 
 export default Header;
