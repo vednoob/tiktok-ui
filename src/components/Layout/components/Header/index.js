078a803eefa27e4fb27995731571ba6d76aa663d
vednoob <leduchyy@gmail.com>
2023-02-12 13:01:35 +0700

    Build UI for header - logic subTippy in Array

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -28,6 +28,21 @@ const MENU_ITEMS = [
   {
     icon: <FontAwesomeIcon icon={faEarthAsia} />,
     title: 'English',
+    children: {
+      title: 'Language',
+      data: [
+        {
+          type: 'language',
+          code: 'en',
+          title: 'English',
+        },
+        {
+          type: 'language',
+          code: 'vi',
+          title: 'Tiếng Việt',
+        },
+      ],
+    },
   },
   {
     icon: <FontAwesomeIcon icon={faCircleQuestion} />,
@@ -46,6 +61,16 @@ function Header() {
   useEffect(() => {
     setsearchResult([]);
   }, []);
+
+  // handle logic
+  const handleMenuChange = (menuItem) => {
+    switch (menuItem.type) {
+      case 'language':
+        // HandleChange Language
+        break;
+      default:
+    }
+  };
 
   return (
     <header className={cx('wrapper')}>
@@ -86,7 +111,7 @@ function Header() {
           <Button text>Upload</Button>
           <Button primary>Log in</Button>
 
-          <Menu items={MENU_ITEMS}>
+          <Menu items={MENU_ITEMS} onChange={handleMenuChange}>
             <button className={cx('more-btn')}>
               <FontAwesomeIcon icon={faEllipsisVertical} />
             </button>
